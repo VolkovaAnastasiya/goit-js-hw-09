ebebb4a5cdaf7a8fd56798b562f4d490d921f71a
VolkovaAnastasiya <volkova.anastasiya1986@gmail.com>
2021-11-23 20:55:18 +0200

    Задача №1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,42 @@
+//инициализация
+const btnStartRef = document.querySelector('button[data-start]');
+const btnStopRef = document.querySelector('button[data-stop]');
+const bodyRef = document.body;
 
+// слушатели событий на кнопки
+
+btnStartRef.addEventListener('click', onClickStart);
+btnStopRef.addEventListener('click', onClickStop);
+
+// ф-ция вешает инлайн стиль backGround на body
+
+function bgColorChench() {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+}
+
+// ф-ция onClickStart  при клике на кноку  запускает setInterval
+
+function onClickStart() {
+  //если кнопа была нажата отключаем ее
+  if (onClickStart) {
+    btnStartRef.disabled = true;
+    btnStopRef.disabled = false;
+  }
+
+  timerId = setInterval(bgColorChench, 1000);
+}
+
+// ф-ция onClickStop  при клике на кноку удаляет setInterval
+
+function onClickStop() {
+  if (onClickStop) {
+    btnStartRef.disabled = false;
+    btnStopRef.disabled = true;
+  }
+
+  clearInterval(timerId);
+} // ф-ция  генерации случайного цвета
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
